b60c39f06b2e4ef30b68d8c015761346d7a1c267
Diarrassouba <diarrassoubazacharia0799@gmail.com>
2026-06-26 16:47:53 +0000

    fix: convert next.config.ts to next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   allowedDevOrigins: [
     '10.10.9.81',
     '10.2.0.2',
     '10.10.9.171',
     '10.10.11.215',
   ],
-}
+};
 
-export default nextConfig
+module.exports = nextConfig;
